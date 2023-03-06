c1af9c7e84149a4d7ae18d46a1d999aebea2b599
newbz521 <lawrence.yee1991@gmail.com>
2023-03-06 16:16:15 -0500

    landing change

--- port2/src/components/cover/cover.js
+++ port2/src/components/cover/cover.js
@@ -15,7 +15,7 @@ function Nav(props) {
   function open() {
     setSlide({ transform: "translateX(-120%)" })
     setInterval(function () { setShift({ height: "0", "transform-origin": "0 0"  });
-    }, 1200)
+    }, 1000)
     // setInterval(function () { setShift({ display: "none", height: "0"})
     // }, 3000)
   }
@@ -24,8 +24,20 @@ function Nav(props) {
     <div className="cover-container" style={shift}>
       <div onClick={open} className="enter-button">
         <div className="metro-card" style={slide}></div>
-        <div className="metro-slot">SWiPE</div>
+        <div className="metro-slot">Swipe to Enter</div>
       </div>
+      <div className="station-tag-wrap">
+        <div className="station-bottom-line"></div>
+        <div className="station-tag"><div className="line-tag"></div><div className="red-stop">Next Stop</div><div className="tag-name next-stop">Lawrence Station</div></div>
+        <div className="station-tag"><div className="line-tag"></div><div className="tag-name">Nutrition</div></div>
+        <div className="station-tag"><div className="line-tag"></div><div className="tag-name">Physical Therapy</div></div>
+        <div className="station-tag"><div className="line-tag"></div><div className="tag-name">Architecture</div></div>
+        <div className="station-tag"><div className="line-tag"></div><div className="tag-name">Software Engineer</div></div>
+      </div>
+      {/* <div className="window-wrap">
+        <div className="window-section"></div>
+        </div> */}
+        {/* <div className="nyc-bg"> Welcome to New York City!</div> */}
   </div>
   );
 }
